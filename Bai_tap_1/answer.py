7abc15ebe272e019b112f55f210416cfa663e2d5
Tu Dinh <tudinhacoustic@gmail.com>
2022-12-26 23:12:37 +0700

    Initial Push

---
+++ Bai_tap_1/answer.py
@@ -0,0 +1,17 @@
+# Bài tập 1
+print("Xin chào, Python")
+
+# Bài tập 2
+print('''Ai về thăm mẹ quê ta
+Chiều nay có đứa con xa nhớ thầm...
+Bầm ơi có rét không bầm!
+Heo heo gió núi, lâm thâm mưa phùn
+Bầm ra ruộng cấy bầm run
+Chân lội dưới bùn, tay cấy mạ non
+Mạ non bầm cấy bầm đon
+Ruột gan bầm lại thương con mấy lần.
+Trích: Bầm ơi - Tố Hữu''')
+
+# Bài tập 3
+print("Area =", 5 * 6)
+print("Perimeter =", (5 + 6) * 2)
